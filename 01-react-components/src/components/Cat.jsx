2035f5a6cdea78efe1a31613fb7b34cd1b650ec3
Marlon Peña <danie1.ts1.hy@gmail.com>
2025-12-06 08:46:21 -0600

    First stemps on react, component excercises

---
+++ 01-react-components/src/components/Cat.jsx
@@ -0,0 +1,13 @@
+const Cat = () => {
+    return (
+        <div className="card">
+            <h2>Mr. Whiskers</h2>
+            <img 
+                src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/running-cats.jpg" 
+                alt="Tuxedo cats running on dirt ground." 
+            />
+        </div>
+    );
+};
+
+export default Cat;
---
+++ 01-react-components/src/components/Greeting.jsx
@@ -0,0 +1,13 @@
+const Greeting = ( {name} ) => {
+
+    // The result will be Hello John
+    return (
+        <>
+            <h1>Hello {name}</h1>
+            <p>Nice to meet you.</p>
+        </>
+    );
+
+};
+
+export default Greeting;
